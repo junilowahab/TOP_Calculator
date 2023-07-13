ceac036629001c2671614c81753eb0872791838e
junilowahab <junilojoshuawahab@gmail.com>
2023-07-13 18:50:07 +0100

    able to evaluate equation but encountered a few errors

--- js/calculator.js
+++ js/calculator.js
@@ -1,67 +1,104 @@
 //linking nodes to the DOM
 let buttons = document.querySelectorAll('.buttons');
+let buttons1 = document.querySelectorAll('.buttons1');
 let display = document.querySelector('#display');
 let operations = document.querySelectorAll('.operator');
+let equalsTo = document.querySelector('#equals');
 
 //user input first value
-let firstValue = parseInt(prompt('Input first value'));
+let firstValue;
 
 //user input operator
-let operator = prompt('Input operator');
+let operator;
 
 //user inputs second value
-let secondValue = parseInt(prompt('Input second value'));
+let secondValue;
 
 
 //arithmetic functions
 function add(a, b){
     console.log(a + b);
+    getFirstValue();
 };
 
 function subtract(a, b){
     console.log(a - b);
+    getFirstValue();
 };
 
 function multiply(a, b){
     console.log(a * b);
+    getFirstValue();
 };
 
 function divide(a, b){
     console.log(a / b);
+    getFirstValue();
 };
 
 //choosing operator
-switch (operator) {
-    case '+':
-        add(firstValue, secondValue)
-        break;
-    case '-':
-        subtract(firstValue, secondValue)
-        break;
-    case '*':
-        multiply(firstValue, secondValue)
-        break;
-    case '/':
-        divide(firstValue, secondValue)
-        break;
-    default:
-        break;
-};
+function solve(){
+    switch (operator) {
+        case '+':
+            add(firstValue, secondValue)
+            break;
+        case '-':
+            subtract(firstValue, secondValue)
+            break;
+        case '*':
+            multiply(firstValue, secondValue)
+            break;
+        case '/':
+            divide(firstValue, secondValue)
+            break;
+        default:
+            break;
+    };
+}
+
 
 //function to display expressions
-function displayExpression(){
+function getFirstValue(){
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             display.innerHTML = display.innerHTML.toString() + button.innerHTML.toString();
-            addOperation();
+            getOperator();
         });
-    })
-
-    function addOperation(){
-        operations.forEach('click', () => {
-            
-        })
-    }
+    });
+    return;
 }
 
-displayExpression();
+function getOperator(){
+    operations.forEach(operation => {
+        operation.addEventListener('click', () => {
+            firstValue = parseInt(display.innerHTML);
+            console.log(firstValue);
+            console.log(typeof(firstValue));
+            display.innerHTML = operation.innerHTML;
+            operator = display.innerHTML.toString();
+            console.log(operator);
+            console.log(typeof(operator));
+            getSecondValue();
+        });
+    });
+    return;
+}
+
+function getSecondValue() {
+    buttons1.forEach(button1 => {
+        button1.addEventListener('click', () => {
+            display.innerHTML = '';
+            display.innerHTML = display.innerHTML.toString() + button1.innerHTML.toString();
+            secondValue = parseInt(display.innerHTML);
+            console.log(secondValue);
+            console.log(typeof(secondValue));
+        });
+    });
+    return;
+}
+
+equalsTo.addEventListener('click', () => {
+    solve();
+})
+
+getFirstValue();
